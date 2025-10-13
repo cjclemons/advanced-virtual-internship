184f8c3917c0bafa4ff16ec197d9dcd905b7e919
cjclemons <carlj.clemons487@gmail.com>
2025-10-13 13:18:43 -0400

    why is it taking so long to load?

---
+++ app/components/home/Navbar.js
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import Logo from "next/image";
+
+function Navbar() {
+  return (
+    <nav className="nav">
+      <div className="nav__wrapper">
+        <figure className="nav__img--mask">
+          <Logo className="nav__img" src="/logo.png" width={500} height={300} />
+        </figure>
+        <ul className="nav__list--wrapper">
+          <li className="nav__list nav__list--login">Login</li>
+          <li className="nav__list nav__list--mobile">About</li>
+          <li className="nav__list nav__list--mobile">Contact</li>
+          <li className="nav__list nav__list--mobile">Help</li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBF5LUgRucvvi_Dsj_tYW4nX4N7aS9DwgA",
+  authDomain: "advanced-virtual-internship.firebaseapp.com",
+  projectId: "advanced-virtual-internship",
+  storageBucket: "advanced-virtual-internship.firebasestorage.app",
+  messagingSenderId: "519132707895",
+  appId: "1:519132707895:web:26eccce9c2c92dee6453b2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
