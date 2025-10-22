935da571b829bf9d4f0a6bdad3598032434b9502
cjclemons <carlj.clemons487@gmail.com>
2025-10-22 16:00:53 -0400

    changed pageprops

--- app/book/[id]/page.tsx
+++ app/book/[id]/page.tsx
@@ -1,17 +1,16 @@
 import InsideBookClient from "@/app/components/book-components/InsideBookClient";
-import { notFound } from "next/navigation";
-import { getBookById } from "../../lib/getBookById";
+import { getBookById } from "@/app/lib/getBookById"; // adjust path if needed
 
 interface PageProps {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }
 
-async function InsideBook({ params }: PageProps) {
+export default async function InsideBook({ params }: PageProps) {
   const book = await getBookById(params.id);
 
   if (!book) return <div>Book not found</div>;
 
   return <InsideBookClient bookId={params.id} />;
 }
-
-export default InsideBook;
